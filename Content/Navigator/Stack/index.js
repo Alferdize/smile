fd3524db3c549a79e06fcc84ebb6d16fd9b9165b
Alferdize <simonandrew132@gmail.com>
2020-05-09 01:35:19 +0530

    Structure

---
+++ Content/Navigator/Stack/index.js
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import { Button, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Login from "../../screens/Auth/Login"
+
+const Stack = createStackNavigator();
+
+function MyStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Login" component={Login} />
+      
+    </Stack.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyStack />
+    </NavigationContainer>
+  );
+}
