515716ea9d45c58c8a7dc72cd9d5180766a15cff
Alferdize <simonandrew132@gmail.com>
2020-05-11 01:55:42 +0530

    Monday

--- Content/screens/Auth/Login/index.js
+++ Content/screens/Auth/Login/index.js
@@ -1,11 +1,9 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { Examples } from '@shoutem/ui';
 
 const index = () => {
     return (
-        <View>
-            <Text style={styles.textstyle}>Login</Text>
-        </View>
+        <Examples />
     )
 }
 
